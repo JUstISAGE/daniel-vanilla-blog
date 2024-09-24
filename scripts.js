b3c903d18240be26addf0c03da8a2a539e481430
Shuhan Lyu <shuhanlyu@ucsb.edu>
2024-09-23 18:49:05 -0700

    implement css

--- scripts.js
+++ scripts.js
@@ -5,10 +5,11 @@ document.getElementById('blog').addEventListener('submit', function(event){
     author = document.getElementById('author').value;
 
 
-    dynamic_content = `<div class=post>
+    dynamic_content = `<div class="post">
                         <h2>${title}</h2>
                         <p><strong>By:</strong> ${author}</p>
                         <p>${paragraph}</p>
+                        </div>
                     `;
     document.getElementById('postedblog').innerHTML = dynamic_content;
     document.getElementById('blog').reset();
