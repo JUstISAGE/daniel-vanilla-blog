d5eff2f8ea692e2a7c3382b0e3bb2423b937808d
Shuhan Lyu <shuhanlyu@ucsb.edu>
2024-09-26 21:09:45 -0700

    final change

--- scripts.js
+++ scripts.js
@@ -1,17 +1,24 @@
-document.getElementById('blog').addEventListener('submit', function(event){
-    event.preventDefault();
+function createnewblog() {
+    var block = document.getElementById("create_blog");
+    block.style.display = "inline";
+}
+function cancelcreate() {
+    var block = document.getElementById("create_blog");
+    block.style.display = "none";
+}
+
+function postnewblog() {
     title = document.getElementById('title').value;
     paragraph = document.getElementById('paragraph').value;
     author = document.getElementById('author').value;
-
-
-    dynamic_content = ` <h2> Posted blogs</h2>
+    dynamic_content = ` 
                         <div class="post">
                         <h2>${title}</h2>
                         <p><strong>By:</strong> ${author}</p>
                         <p>${paragraph}</p>
                         </div>
                     `;
-    document.getElementById('postedblog').innerHTML = dynamic_content;
-    document.getElementById('blog').reset();
-})
+    document.getElementById('postedblog').innerHTML += dynamic_content;
+    var block = document.getElementById("create_blog");
+    block.style.display = "none";
+}
